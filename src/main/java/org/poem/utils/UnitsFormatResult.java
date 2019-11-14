9b05538199a3b4759936f6177a0c615d9b65b5d9
lywwrking <xue_2013@sina.com>
2019-11-15 00:32:09 +0800

    commit first

---
+++ src/main/java/org/poem/utils/UnitsFormatResult.java
@@ -0,0 +1,37 @@
+package org.poem.utils;
+
+/**
+ * @author wangzhen3
+ * @version 2018/1/12
+ */
+public class UnitsFormatResult {
+    private String value;//值
+
+    private String suffix;//后缀  value + " " + suffix  组成 "25 %"
+
+    private String adaptedUnits;//最合适的单位
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public String getAdaptedUnits() {
+        return adaptedUnits;
+    }
+
+    public void setAdaptedUnits(String adaptedUnits) {
+        this.adaptedUnits = adaptedUnits;
+    }
+}
